5c389a6f1759c63ed176682bdfc5b8ee5986d4af
luiz-limaribeiro <de.limaribeiroluiz@gmail.com>
2025-11-25 21:21:43 -0300

    feat: add the featured songs section

--- web/src/pages/LandingPage.tsx
+++ web/src/pages/LandingPage.tsx
@@ -1,14 +1,29 @@
 import Navbar from "../components/ui/Navbar";
 import Playback from "../components/ui/Playback";
 import SongCard from "../components/ui/SongCard";
-import './LandingPage.css'
+import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
     <main className="landing-page">
-      <h1>Music Workstation</h1>
+      <h1>
+        Music <br />
+        Workstation
+      </h1>
       <p>Create music in the browser</p>
-      <SongCard />
+      <section className="featured-songs">
+        <h3>featured songs</h3>
+        <ul>
+          {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+            <li key={i}>
+              <SongCard />
+            </li>
+          ))}
+        </ul>
+      </section>
+      <div className="call">
+        <button>start exploring</button>
+      </div>
       <Playback />
       <Navbar />
     </main>
